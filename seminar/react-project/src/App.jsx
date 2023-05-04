486889483ccf98f9f0ad1bf15b4c9aec0af12a37
seobbang <seobbangway@naver.com>
2023-05-04 22:19:01 +0900

    3주차 세미나 + setState 공부

--- seminar/react-project/src/App.jsx
+++ seminar/react-project/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// setState 동작 방식 이해하기
+export default function App() {
+  const [number, setNumber] = useState(1);
 
-function App() {
-  const [count, setCount] = useState(0)
+  // const add = () => setNumber(number + 1);
+  // const subtract = () => setNumber(number - 1);
+  // const multiplyBy2 = () => setNumber(number * 2);
+  // const multiplyBy2AndAddBy1 = () => {
+  //   multiplyBy2();
+  //   add();
+  // };
+
+  const add = () => setNumber((number) => number + 1);
+  //2. 그 다음에 넘겨받은 인자의 함수도 아까 그 큐에 넣는다.
+  const subtract = () => setNumber((number) => number - 1);
+  const multiplyBy2 = () => setNumber((number) => number * 2);
+  //1. setNumber가 함수를 넘겨받으면 이걸 나중에 실행 해야지 하고 큐라는 구조를 가지고 있는 저장 공간에 넣는다.
+  const multiplyBy2AndAddBy1 = () => {
+    multiplyBy2();
+    add();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="outer-wrapper">
+      <h1>Number : {number}</h1>
+      <div className="btn-wrapper">
+        <button onClick={add}>+ 1</button>
+        <button onClick={subtract}>- 1</button>
+        <button onClick={multiplyBy2}>*2</button>
+        <button onClick={multiplyBy2AndAddBy1}>*2 + 1</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
