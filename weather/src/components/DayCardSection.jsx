35b5d3413bdbbf163ab8baff69be40c5db1934e2
seobbang <seobbangway@naver.com>
2023-05-11 03:24:07 +0900

    feat: 오늘 날씨 불러오기 API 통신 구현

--- weather/src/components/DayCardSection.jsx
+++ weather/src/components/DayCardSection.jsx
@@ -1,10 +1,33 @@
 import WeatherCard from "./WeatherCard";
 import styled from "styled-components";
 
+import axios from "axios";
+
+import { useParams } from "react-router-dom";
+
 const DayCardSection = () => {
+  const { area } = useParams();
+  const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${
+    import.meta.env.VITE_APP_WEATHER
+  }&units=metric`;
+
+  const getWeatherInfo = async () => {
+    axios
+      .get(WEATHER_URL)
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const { data } = getWeatherInfo();
+
   return (
     <St.CardContainer>
-      <WeatherCard />
+      <WeatherCard data={data} />
     </St.CardContainer>
   );
 };
