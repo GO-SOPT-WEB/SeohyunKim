07383312483770f3263dc7958e84c3a9d60c6004
seobbang <seobbangway@naver.com>
2023-05-11 04:34:42 +0900

    feat: 주간 날씨 정보 API 통신 구현

--- weather/src/components/WeekCardSection.jsx
+++ weather/src/components/WeekCardSection.jsx
@@ -1,13 +1,49 @@
-import styled from "styled-components";
 import WeatherCard from "./WeatherCard";
+import styled from "styled-components";
+
+import axios from "axios";
+
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const WeekCardSection = () => {
-  return (
-    <St.CardContainer>
-      <WeatherCard />
-      <WeatherCard />
-    </St.CardContainer>
-  );
+  const { area } = useParams();
+  const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${
+    import.meta.env.VITE_APP_WEATHER
+  }&units=metric`;
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  const getWeatherInfo = async () => {
+    axios
+      .get(WEATHER_URL)
+      .then((res) => {
+        const {
+          data: { list },
+        } = res;
+        setWeatherData(list);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // area 바뀌면 검색
+  useEffect(() => {
+    getWeatherInfo();
+  }, [area]);
+
+  const renderData = weatherData?.filter((data) => {
+    const dateTime = data.dt_txt.split(" ");
+    if (dateTime[1] === "21:00:00") {
+      return true;
+    }
+  });
+  const WeatherCardList = renderData?.map((data) => {
+    return <WeatherCard key={data.dt_txt} data={data} />;
+  });
+
+  return <St.CardContainer>{WeatherCardList}</St.CardContainer>;
 };
 
 export default WeekCardSection;
