3a64c7593ea7d34251190608941e2bf26546b6e8
seobbang <seobbangway@naver.com>
2023-06-10 01:22:50 +0900

    fix: 타입 단언

--- find-ompangi-ts/src/main.tsx
+++ find-ompangi-ts/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 
-ReactDOM.createRoot(document.getElementById("app-root")).render(
+ReactDOM.createRoot(document.getElementById("app-root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
