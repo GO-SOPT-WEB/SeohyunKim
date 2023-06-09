6960eb56835e9b2bbf1d67f28275453e9c659bda
seobbang <seobbangway@naver.com>
2023-06-10 01:55:48 +0900

    feat: RecoilRoot 적용

--- find-ompangi-ts/src/App.tsx
+++ find-ompangi-ts/src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Router from "./components/Router";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </RecoilRoot>
     </>
   );
 }
